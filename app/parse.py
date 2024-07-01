4425311fe6d2b2796b23fa277351b3c2851afc14
charm <charmniovi@tutanota.com>
2024-07-01 17:01:17 +0300

    solution

--- app/parse.py
+++ app/parse.py
@@ -1,10 +1,9 @@
-import asyncio
 import csv
-from dataclasses import dataclass, fields
-from time import time
+from dataclasses import dataclass, fields, asdict
+from datetime import date, datetime
 from urllib.parse import urljoin
-import aiofiles
-import aiohttp
+
+import requests
 from bs4 import BeautifulSoup
 
 BASE_URL = "https://quotes.toscrape.com/"
@@ -16,62 +15,95 @@ class Quote:
     author: str
     tags: list[str]
 
-    def to_dict(self):
-        return {
-            field.name: getattr(self, field.name)
-            for field in fields(self)
-        }
+
+@dataclass
+class Author:
+    name: str
+    bio: str
+    born_date: date
+    born_location: str
 
 
-async def parse_url_for_quote(
-        url: str,
-        session: aiohttp.ClientSession
-) -> list[Quote] | None:
-    async with session.get(url) as response:
-        if response.status != 200:
-            return None
-        soups = BeautifulSoup(await response.text(), "html.parser")
-        quote_list = [
-            Quote(
-                text=soup.select_one(".text").text,
-                author=soup.select_one(".author").text,
-                tags=[tag.text for tag in soup.select(".tag")],
-            ) for soup in soups.select(".quote")
-        ]
-        return quote_list
+def write_csv(output_csv_path: str, data_list: list, headers: list) -> None:
+    with open(output_csv_path, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, headers)
+        writer.writeheader()
+        for obj in data_list:
+            writer.writerow(asdict(obj))
 
 
-async def parse_all_quotes() -> list[Quote]:
+def parse_quote(soups: BeautifulSoup) -> list[Quote]:
+    return [
+        Quote(
+            text=quote.select_one(".text").text,
+            author=quote.select_one(".author").text,
+            tags=[tag.text for tag in quote.select(".tag")]
+        )
+        for quote in soups.select(".quote")
+    ]
+
+
+def parse_authors_href(soups: BeautifulSoup) -> set:
+    return {
+        author_span.select_one("a")["href"]
+        for author_span in soups.select("span:has(small.author)")
+    }
+
+
+def parse_all_author(authors_href: set) -> list[Author]:
+    all_authors = []
+    for href in authors_href:
+        response = requests.get(urljoin(BASE_URL, href))
+        if response.status_code != 200:
+            continue
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        author_details = soup.select_one(".author-details")
+        all_authors.append(Author(
+            name=author_details.select_one(".author-title").text,
+            bio=author_details.select_one(".author-description").text,
+            born_location=author_details.select_one(
+                ".author-born-location"
+            ).text,
+            born_date=datetime.strptime(
+                author_details.select_one(".author-born-date").text,
+                "%B %d, %Y"
+            )
+        ))
+    return all_authors
+
+
+def parse_page(url: str) -> tuple[list[Quote], set] | None:
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+
+    soups = BeautifulSoup(response.text, "html.parser")
+    if soups.select_one(".quote"):
+        return parse_quote(soups), parse_authors_href(soups)
+
+
+def main(
+        quote_csv_path: str = "quotes.csv",
+        author_csv_path: str = "authors.csv"
+) -> None:
     page = 1
     all_quotes = []
-    async with aiohttp.ClientSession() as session:
-        while quote_list := await parse_url_for_quote(
-                urljoin(BASE_URL, f"/page/{page}/"),
-                session
-        ):
-            page += 1
-            all_quotes.extend(quote_list)
-    return all_quotes
+    unique_authors = set()
+    quotes_fields = [field.name for field in fields(Quote)]
+    author_fields = [field.name for field in fields(Author)]
+    while response := parse_page(
+            urljoin(BASE_URL, f"/page/{page}/")
+    ):
+        quote, authors_href = response
+        all_quotes.extend(quote)
+        unique_authors.update(authors_href)
+        page += 1
 
-
-async def create_and_fill_csv(
-        quote_list: list[Quote],
-        output_csv_path: str
-) -> None:
-    async with aiofiles.open(output_csv_path, "w", newline='') as csvfile:
-        writer = csv.DictWriter(
-            csvfile,
-            [field.name for field in fields(Quote)]
-        )
-        await writer.writeheader()
-        await asyncio.gather(*[writer.writerow(quote.to_dict()) for quote in quote_list])
-
-
-async def main(output_csv_path: str) -> None:
-    await create_and_fill_csv(await parse_all_quotes(), output_csv_path)
+    all_author = parse_all_author(unique_authors)
+    write_csv(quote_csv_path, all_quotes, quotes_fields)
+    write_csv(author_csv_path, all_author, author_fields)
 
 
 if __name__ == "__main__":
-    start = time()
-    asyncio.run(main("quotes.csv"))
-    print(time() - start)
+    main("quotes.csv")
